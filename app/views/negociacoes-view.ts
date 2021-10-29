af2c5ddb10c007568d35589e15b95bdecdb7b0de
FelipePilz <felipevilvertpilz@gmail.com>
2021-10-29 20:48:17 -0300

    Adicionei protected aos métodos template()

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -2,7 +2,7 @@ import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         if (model.lista().length == 0) {
             return ``;
         }
