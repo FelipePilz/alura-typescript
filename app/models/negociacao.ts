3c93fa86f1decf1ae22337bec4a53e225d09f083
FelipePilz <felipevilvertpilz@gmail.com>
2021-10-31 16:20:59 -0300

    Movido o método de criar negociação para a instância de negociação

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -14,4 +14,16 @@ export class Negociacao {
         //Clonando a data
         return data;
     }
+
+    public criaDe(
+        dataString: string,
+        quantidadeString: string,
+        valorString: string
+    ): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ","));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -12,4 +12,11 @@ export class Negociacao {
         //Clonando a data
         return data;
     }
+    criaDe(dataString, quantidadeString, valorString) {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ","));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
 }
