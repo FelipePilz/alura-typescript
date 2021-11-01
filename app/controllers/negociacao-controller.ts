e1a4c85e612dd16a5c6b6eca84a0780c8909e217
FelipePilz <felipevilvertpilz@gmail.com>
2021-10-31 23:13:54 -0300

    Suprimindo os erros com null no controller de negociação

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -13,9 +13,9 @@ export class NegociacaoController {
     private mensagemView = new MensagemView("#mensagemView");
 
     constructor() {
-        this.inputData = document.querySelector("#data");
-        this.inputQuantidade = document.querySelector("#quantidade");
-        this.inputValor = document.querySelector("#valor");
+        this.inputData = document.querySelector("#data") as HTMLInputElement;
+        this.inputQuantidade = document.querySelector("#quantidade") as HTMLInputElement;
+        this.inputValor = document.querySelector("#valor") as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
 
@@ -26,9 +26,7 @@ export class NegociacaoController {
             this.inputValor.value
         );
         if (!this.diaUtil(negociacao.data)) {
-            this.mensagemView.update(
-                "Apenas negociações em dias úteis são aceitas!"
-            );
+            this.mensagemView.update("Apenas negociações em dias úteis são aceitas!");
             return; //Early return
         }
 
@@ -39,10 +37,7 @@ export class NegociacaoController {
 
     private diaUtil(data: Date): boolean {
         // 0 - Domingo \ 6 - Sabado
-        return (
-            data.getDay() > DiasDaSemana.DOMINGO &&
-            data.getDay() < DiasDaSemana.SABADO
-        );
+        return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
     }
 
     private limparFormulario(): void {
